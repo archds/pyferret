38ae4cf906fa242bf7d699a989e8b248123637c4
archds <dsalekseev11@gmail.com>
2023-07-08 00:26:30 +0700

    Update imports

--- src/pyfunc/__init__.py
+++ src/pyfunc/__init__.py
@@ -1,6 +1,16 @@
-import abstract
-
+from .abstract import Applicative, Context, Functor, Monad
 from .maybe import Just, Maybe, Nothing
 from .result import Err, Ok, Result
 
-__all__ = ["Just", "Nothing", "Maybe", "Ok", "Err", "Result", "abstract"]
+__all__ = [
+    "Just",
+    "Nothing",
+    "Maybe",
+    "Ok",
+    "Err",
+    "Result",
+    "Context",
+    "Functor",
+    "Applicative",
+    "Monad",
+]
